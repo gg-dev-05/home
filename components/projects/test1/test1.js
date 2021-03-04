4570cc7b5a69e6c13eb746894b8f00c757197be0
= <garvitgalgat@gmail.com>
2021-03-04 14:48:39 +0530

    test project

--- components/projects/test1/test1.js
+++ components/projects/test1/test1.js
@@ -1,12 +1,23 @@
 import React from 'react'
+import Section from '../../section/section';
+import Page from '../../page/page';
 import './test1.scss';
-
+import { ImQuotesLeft, ImQuotesRight } from 'react-icons/im'
 const Test1 = () => {
     return (
-        <div className="test1">
-            <div className="heading"><b>TESTING</b></div>
-            <article className="body">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Numquam, reprehenderit sed voluptates culpa minima iste velit adipisci illo libero odio, d</article>
-        </div>
+        <Page >
+            <Section backgroundColor="#0f0e17">
+                <div className="test1">
+                    <div className="heading"><b>CLUBS IITI</b></div>
+                    <article className="small-desc">A club management system build for <strong>IITI</strong> community.</article>
+                    <h2>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Autem incidunt maiores, quaerat adipisci quis quibusdam corrupti ipsam dolorum libero molestiae corporis culpa, modi maxime obcaecati. Illo pariatur natus quaerat nesciunt?</h2>
+                    <h2>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Autem incidunt maiores, quaerat adipisci quis quibusdam corrupti ipsam dolorum libero molestiae corporis culpa, modi maxime obcaecati. Illo pariatur natus quaerat nesciunt?</h2>
+                    <h2>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Autem incidunt maiores, quaerat adipisci quis quibusdam corrupti ipsam dolorum libero molestiae corporis culpa, modi maxime obcaecati. Illo pariatur natus quaerat nesciunt?</h2>
+                </div>
+            </Section>
+
+        </Page>
+
     )
 }
 
