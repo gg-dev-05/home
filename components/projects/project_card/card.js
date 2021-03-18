a367aee7c531d173c6d43523dd8a909c392677ef
gg-dev-05 <garvitgalgat@gmail.com>
2021-03-19 01:59:16 +0530

    image transitions (project page)

--- components/projects/project_card/card.js
+++ components/projects/project_card/card.js
@@ -3,15 +3,19 @@ import React from 'react'
 import './card.scss'
 
 export const Card = ({image, text}) => {
-    const image1 = "https://picsum.photos/id/237/400/300"
-    const image2 = "https://picsum.photos/id/238/400/300"
+    const image1 = "https://res.cloudinary.com/dkvh16kiu/image/upload/w_400,h_300,c_scale/v1616093823/image1_kwjm2u.webp"
+    const image2 = "https://res.cloudinary.com/dkvh16kiu/image/upload/w_400,h_300,c_scale/v1616093824/image2_zwwfxs.webp"
     text = "enean iaculis ut mauris quis efficitur. Vestibulum ante ipsum primis i"
     return (
         <div className="project_card" >
             <img className="back_image" src={image1}/>
             <div className="front_image_frame">
                 <img className="front_image" src={image2}/>
+                <div className="project-text">
+                    {text}
+                </div>
             </div>
+            
         </div>
     )
 }
