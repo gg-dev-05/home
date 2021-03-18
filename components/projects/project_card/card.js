4e4d84fca24b7ef07c94dfe9e5743b8dc13eab59
gg-dev-05 <garvitgalgat@gmail.com>
2021-03-19 00:10:06 +0530

    animations done for projects section

--- components/projects/project_card/card.js
+++ components/projects/project_card/card.js
@@ -1,3 +1,4 @@
+import { urlObjectKeys } from 'next/dist/next-server/lib/utils'
 import React from 'react'
 import './card.scss'
 
@@ -6,8 +7,11 @@ export const Card = ({image, text}) => {
     const image2 = "https://picsum.photos/id/238/400/300"
     text = "enean iaculis ut mauris quis efficitur. Vestibulum ante ipsum primis i"
     return (
-        <div className="project_card" style={{background: `url(${image1})`}}>
-            
+        <div className="project_card" >
+            <img className="back_image" src={image1}/>
+            <div className="front_image_frame">
+                <img className="front_image" src={image2}/>
+            </div>
         </div>
     )
 }
